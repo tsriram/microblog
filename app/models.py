21ca5207c8680d5951293e19c51926b0f632bcd1
Sriram Thiagarajan <sri.sjc@gmail.com>
2016-07-29 14:18:37 +0530

    part 10 - full text search

--- app/models.py
+++ app/models.py
@@ -1,5 +1,12 @@
-from app import db
+from app import app, db
 from hashlib import md5
+
+import sys
+if sys.version_info >= (3, 0):
+    enable_search = False
+else:
+    enable_search = True
+    import flask.ext.whooshalchemy as whooshalchemy
 
 followers = db.Table('followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
@@ -73,6 +80,8 @@ class User(db.Model):
         return '<User %r>' % (self.nickname)
 
 class Post(db.Model):
+    __searchable__ = ['body']
+
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime)
@@ -80,3 +89,6 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %r>' % (self.body)
+
+if enable_search:
+    whooshalchemy.whoosh_index(app, Post)
--- config.py
+++ config.py
@@ -25,3 +25,7 @@ ADMINS = ['you@example.com']
 
 # pagination
 POSTS_PER_PAGE = 3
+
+WHOOSH_BASE = os.path.join(basedir, 'search.db')
+
+MAX_SEARCH_RESULTS = 50
